6a39f3d64791f93af2edea1eee5798b99fd78603
Jeanne Coleongco <coleongc@ualberta.ca>
2020-09-25 10:52:30 -0600

    chks valid positions / usr input, cont'd gameplay

--- connect6.py
+++ connect6.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import sys
+import pygame
+import numpy as np
+import math
 
 board = []
 for x in range(19):
@@ -19,37 +22,60 @@ print_board(board)
 
 # even for black, odd for white
 turn = 0
+game_over = False
 
 def choose():
-	choice = input("0 to choose Black, 1 to choose White: ")
+	choice = int(input("0 to choose Black, 1 to choose White: "))
+	while choice != 0 and choice != 1:
+		choice = int(input("0 to choose Black, 1 to choose White: "))
 	turn = choice
 
 first = True
 
+rows_played = set()
+cols_played = set()
+
 def play(board, first):
 	if first:
 		r, c = input().split()
+		while (r not in coords or c not in coords): r, c = input().split()
+		while (r in rows_played and c in cols_played): 
+			r, c = input().split()
+		
 		row = coords.index(r)
 		col = coords.index(c)
-
 		board[row][col] = "X"
+		rows_played.add(coords[row])
+		cols_played.add(coords[col])
 		print_board(board)
-		first = False
 	else:
 		for i in range(2):
 			r, c = input().split()
+			while (r not in coords or c not in coords): r, c = input().split()
+			while (r in rows_played and c in cols_played): 
+				r, c = input().split()
+			
 			row = coords.index(r)
 			col = coords.index(c)
 
-			board[row][col] = "X"
+			if turn%2 == 0: board[row][col] = "X"
+			else: board[row][col] = "O"
+			rows_played.add(coords[row])
+			cols_played.add(coords[col])
 		print_board(board)
 
 choose()
 
-# need to go back and forth between while loops
-while turn%2 == 0:
-	play(board, first)
-	turn += 1
-#while turn%2 == 1:
-	# call player algorithm
-	# turn += 1
+#def chk_win(board):
+
+
+while not game_over:
+	print(rows_played)
+	print(cols_played)
+	if turn%2 == 0:
+		play(board, first)
+		turn += 1
+	else:
+		play(board, first)
+		turn += 1
+	first = False
